16d34bc9a806a9d9f0bc2f487968354cb9c809db
Philco Michel <phil9231@gmail.com>
2016-12-20 16:05:39 -0500

    update assets and post access

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,7 @@ Rails.application.routes.draw do
   resources :posts
   devise_for :users, controlles: { registrations: "registrations"}
 
+
+  get 'home/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
