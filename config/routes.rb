e3a66f36656df83cb27db8f8f3d69aa7bcc61d4c
Philco Michel <phil9231@gmail.com>
2016-12-27 22:49:06 -0500

    video now reposive

--- config/routes.rb
+++ config/routes.rb
@@ -8,11 +8,12 @@ Rails.application.routes.draw do
 
   resources :events
   get 'events/index'
-
+  get 'home/index'
   get 'home/sat'
    get 'home/event'
     get 'home/prayer'
      get 'home/testemony'
      get 'home/fun'
+     get 'home/out'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
