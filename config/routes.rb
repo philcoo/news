b44052d203414826ba1e03f9562e45535f8fd8b9
Philco Michel <phil9231@gmail.com>
2016-12-30 00:13:01 -0500

    trying video responsive

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
 
+  get 'funs/index'
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   root 'home#index'
 
@@ -15,5 +16,6 @@ Rails.application.routes.draw do
      get 'home/testemony'
      get 'home/fun'
      get 'home/out'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161221010856) do
+ActiveRecord::Schema.define(version: 20161229195102) do
 
   create_table "events", force: :cascade do |t|
     t.string   "suject"
@@ -18,6 +18,13 @@ ActiveRecord::Schema.define(version: 20161221010856) do
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
     t.integer  "user_id"
+  end
+
+  create_table "funs", force: :cascade do |t|
+    t.string   "title"
+    t.text     "contemt"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "posts", force: :cascade do |t|
